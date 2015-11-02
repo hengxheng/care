ddf5e5683bf9cd76966c9d75bb6de053125f5b9a
hengxheng <hengxheng@hotmail.com>
2015-11-02 16:16:39 +1100

    job list style

--- resources/views/job/list.blade.php
+++ resources/views/job/list.blade.php
@@ -11,17 +11,18 @@
             <li>
             	<div class="job-block">
                     <div class="job-title">
-                    	{{ $job -> title }}
+                    	{{ $job -> title }} (<span class="{{ str_slug($job->status) }}">{{ $job->status }}</span>)
                     </div>
                     <div class="job-desc">
                     	{{ $job -> description }}
                     </div>
                     <div class="job-status">
-                    	{{ $job -> status }}
+                    	
                     </div>
                     <div class="job-action">
-                    	<a href="{{ URL::route('job.edit', array('id' => $job->id )) }}">Edit</a>
-                    	<a href="{{ URL::route('job.delete', array('id' => $job->id )) }}">Delete</a>
+                        <a class="blue-btn" href="{{ URL::route('job.show', array('id' => $job->id )) }}">View</a>
+                    	<a class="blue-btn" href="{{ URL::route('job.edit', array('id' => $job->id )) }}">Edit</a>
+                    	<a class="dark-blue-btn" href="{{ URL::route('job.delete', array('id' => $job->id )) }}">Delete</a>
                     </div>
                 </div>
             </li>
