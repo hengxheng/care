c7a8d56d2dd46b2937d5eaaa7fbd5ceb0eae7a5a
hengxheng <hengxheng@hotmail.com>
2015-11-13 09:29:42 +1100

    giver list style

--- resources/views/giver/list.blade.php
+++ resources/views/giver/list.blade.php
@@ -15,18 +15,27 @@
                          <div class="user-name">
                              {{ $giver->firstname }} {{ $giver->lastname}}
                          </div>
+                         <div class="user-location">
+                             {{ $giver->suburb }},{{ $giver->state }}
+                         </div>
                          <div class="user-exp">
                              {{ str_limit($giver->experience, 200) }}
-                         </div>
-                         <div class="user-rate">
-                             Rate: ${{ $giver->rate }}
                          </div>
                          <div class="cta">
                             <a class="blue-btn" href="{{ URL::route('care_givers.show', array('uid' => $giver->id )) }}">View</a>
                             <a class="dark-blue-btn" href="{{ URL::route('message.create', array('to_id'=>$giver->id )) }}">Send a message</a>
                         </div>
                      </div>
-                     
+                     <div class="user-right-box">
+                        <div class="user-rate">
+                            <h2>${{ $giver->rate }}</h2>
+                            <p>per hour</p>
+                        </div>
+                        <div class="year-exp">
+                            <h2>0</h2>
+                            <p>years<br/>experience</p>
+                        </div>
+                     </div>
                 </div>              
             </li>
         @endforeach
