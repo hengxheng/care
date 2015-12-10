2e9f473df1ba191ab37ad31f0f323639609ca2e0
hengxheng <hengxheng@hotmail.com>
2015-12-10 15:35:32 +1100

    location table

---
+++ database/migrations/2015_12_10_041557_create_location_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLocationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('location', function (Blueprint $table) {
+             $table->string('countrycode');
+            $table->string('region1');
+            $table->string('region2');
+            $table->string('region3');
+            $table->string('postcode');
+            $table->string('locality');
+            $table->decimal('latitude',13,9);
+            $table->decimal('longitude',13,9);
+            $table->string('iso');
+            $table->string('fips');
+            $table->string('hasc');
+            $table->string('stat');
+            $table->string('timezone');
+            $table->string('utc');
+            $table->string('dst');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('location');
+    }
+}
