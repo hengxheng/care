1f0bd931c0dc7957381f181b7f93eedb731d1ef1
hengxheng <hengxheng@hotmail.com>
2016-02-08 17:18:52 +1100

    Major changes to the CSS/SASS I have restructed them in to partial documnents sop there is no need to the endless scrolling
    Changed the login page to include both the register forms and the login. No point on having them on spearte changes when they take up so little room
    
    Started work on the profile page, adding icons and make it seem more of a design

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,15 +1,24 @@
 @extends('html')
+@section('pageType', 'home-page')
+@endsection
 
 @section('content')
+
+<div class="welcome title">
+    <h1>Welcome to CareNation</h1>
+</div>
+
+<div class="login-block">
+    <h1>Login to CareNation</h1>
 <form method="POST" action="{{ URL::route('login') }}">
     {!! csrf_field() !!}
 
-    <div class="form-row">
+    <div class="form-row first">
         <label for="email">Email</label>
         <input type="email" name="email" value="{{ old('email') }}">
     </div>
 
-    <div class="form-row">
+    <div class="form-row second">
         <label for="password">Password</label>
         <input type="password" name="password" id="password">
     </div>
@@ -22,5 +31,34 @@
         <input type="submit" value="Login">
     </div>
 </form>
+</div>
+
+<div class="register-block-container">
+    <div class="register-block">
+    <h1>Find Care For Your Family Today!</h1>
+    <p>Get started by posting your care needs.</p>
+    <a href="{{ URL::route('seeker_register') }}" class="blue-btn">I'm looking for care</a>
+</div>
+
+<div class="register-block">
+    <h1>Apply for Caregiver Jobs</h1>
+    <p>Connect with those seeking care</p>
+    <a href="{{ URL::route('giver_register') }}" class="blue-btn">I want to provide care</a>
+</div>
+</div>
+
+<div class="testimonial-block">
+    <h1>What our users think</h1>
+    <div class="single-testimonail">
+        <div class="testimonial-featured">
+            <img src="https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg" style="border-radius: 64px;" />
+        </div>
+
+        <div class="testimonial-content">
+            <h3>J. Casey</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec interdum leo id tempor mollis. Nam elit tellus, finibus sit amet aliquam ut, venenatis at diam. Fusce ut dui vel lacus pulvinar auctor non ut mauris. Maecenas sit amet turpis eget eros luctus semper. Maecenas tempor odio id massa aliquam, id hendrerit nisl convallis.</p>
+        </div>
+    </div>
+</div>
 
 @endsection
--- resources/views/seeker/show.blade.php
+++ resources/views/seeker/show.blade.php
@@ -14,29 +14,32 @@
 					</h2>
 				
 					<div class="block-content">
+						<h3>
 						@if($the_seeker->subscription_ends_at == null)
 						Monthly Subcription
 						@else
 						Subscrtipion ends: {{ $the_seeker->subscription_ends_at }}
 						@endif
+					</h3>
 					</div>
+
+					<div class="block-content contact">
+						<h3 class="block-title">Contact Information</h3>
+						<div class="block-content">
+							<ul>
+								<li class="email"><p><span>Email:</span>{{ $the_user->email }}</p></li>
+								<li class="phone"><p><span>Phone: </span>{{ $the_user->phone }}</p></li>
+								<li class="address"><p><span>Address: </span>{{ $the_seeker->address1 }}</p></li>
+								<li class="address2"><p><span>Address2: </span>{{ $the_seeker->address2 }}</p></li>
+								<li class="suburb"><p><span>Suburb: </span>{{ $the_seeker->suburb }}</p></li>
+								<li class="state"><p><span>State: </span>{{ $the_seeker->state }}</p></li>
+								<li class="postcode"><p><span>Postcode: </span>{{ $the_seeker->postcode }}</p></li>
+							</ul>
+						</div>
+					</div>
+
+					<a class="edit-btn" href="{{ URL::route('care_seekers.edit', array('id'=>$the_user->id)) }}">Edit</a>
 		
-				</div>
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-1">
-				<div class="profile-contact block">
-					<h2 class="block-title">Contact <a class="edit-btn" href="{{ URL::route('care_seekers.edit', array('id'=>$the_user->id)) }}">Edit</a></h2>
-					<div class="block-content">
-						<p>Email: {{ $the_user->email }}</p>
-						<p>Phone: {{ $the_user->phone }}</p>
-						<p>Address: {{ $the_seeker->address1 }}</p>
-						<p>Address2: {{ $the_seeker->address2 }}</p>
-						<p>Suburb: {{ $the_seeker->suburb }}</p>
-						<p>State: {{ $the_seeker->state }}</p>
-						<p>Postcode: {{ $the_seeker->postcode }}</p>
-					</div>
 				</div>
 			</div>
 		</div>
