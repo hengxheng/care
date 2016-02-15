06060c82d29039798d91183a8f650e307bf14ed5
hengxheng <hengxheng@hotmail.com>
2016-02-15 17:18:31 +1100

    Change of responsive meta tag

--- resources/views/html.blade.php
+++ resources/views/html.blade.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title>Care Nation</title>
 		<link rel="stylesheet" href="{{ URL::asset('style.css') }}">
 		<link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
