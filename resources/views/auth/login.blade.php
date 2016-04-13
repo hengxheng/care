97e18b92d257e8bba99aa5d5241f376f3c37edec
hengxheng <hengxheng@hotmail.com>
2016-04-13 15:17:07 +1000

    seperate login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,4 @@
 @extends('html')
-@section('pageType', 'home-page')
-@endsection
 
 @section('content')
 
@@ -8,7 +6,7 @@
     <h1>Welcome to CareNation</h1>
 </div>
 
-<div class="login-block">
+<div class="login-block" style="padding-left:0;">
     <h1>Login to CareNation</h1>
 <form method="POST" action="{{ URL::route('login') }}">
     {!! csrf_field() !!}
@@ -30,36 +28,9 @@
     <div class="form-row submit">
         <input type="submit" value="Login">
     </div>
+
 </form>
-</div>
-
-<div class="register-block-container">
-    <div class="register-block">
-    <h1>Find Care For Your Family</h1>
-    <p>Get started by posting your care needs today and see who can hep.<br/>&nbsp;</p>
-    <!-- <a href="{{ URL::route('seeker_register') }}" class="blue-btn">I'm looking for care</a> -->
-    <a href="http://www.carenation.com.au/careseeker_signup/" class="blue-btn">I'm looking for care</a>
-</div>
-
-<div class="register-block">
-    <h1>Apply for Caregiver Jobs</h1>
-    <p>Connect with those seeking care today and see how you can help people in need.</p>
-    <a href="{{ URL::route('giver_register') }}" class="blue-btn">I want to provide care</a>
-</div>
-</div>
-
-<div class="testimonial-block">
-    <h1>What our users think</h1>
-    <div class="single-testimonail">
-        <div class="testimonial-featured">
-            <img src="{{ URL::asset('images/fiverr.jpg') }}" style="border-radius: 50%;width: 128px; height: 128px;" />
-        </div>
-
-        <div class="testimonial-content">
-            <h3>Nancy</h3>
-            <p>I am loving CareNation, it is a very easy website to navigate and to get the help we need for our Mother. Neither my sister or I live close enough to Mum to be there to help her daily so we needed to find someone to be there for Mum. CareNation can provide just that! </p>
-        </div>
-    </div>
+    <p id="join-link">Not a member? Click here to <a href="{{ URL::route('register') }}">Join Now</a>.</p>
 </div>
 
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,16 +1,40 @@
 @extends('html')
+@section('pageType', 'home-page')
+@endsection
 
 @section('content')
 
-<div class="register-block">
-    <h2>Find Care For Your Family Today!</h2>
-    <p>Get started by posting your care needs.</p>
-    <a href="{{ URL::route('seeker_register') }}" class="blue-btn">I'm looking for care</a>
+<div class="welcome title">
+    <h1>Welcome to CareNation</h1>
 </div>
 
-<div class="register-block">
-	<h2>Apply for Caregiver Jobs</h2>
-	<p>Connect with those seeking care</p>
-	<a href="{{ URL::route('giver_register') }}" class="blue-btn">I want to provide care</a>
+<div class="register-block-container">
+    <div class="register-block">
+        <h1>Find Care For Your Family</h1>
+        <p>Get started by posting your care needs today and see who can hep.<br/>&nbsp;</p>
+        <!-- <a href="{{ URL::route('seeker_register') }}" class="blue-btn">I'm looking for care</a> -->
+        <a href="http://www.carenation.com.au/careseeker_signup/" class="blue-btn">I'm looking for care</a>
+    </div>
+
+    <div class="register-block">
+        <h1>Apply for Caregiver Jobs</h1>
+        <p>Connect with those seeking care today and see how you can help people in need.</p>
+        <a href="{{ URL::route('giver_register') }}" class="blue-btn">I want to provide care</a>
+    </div>
 </div>
+
+<div class="testimonial-block">
+    <h1>What our users think</h1>
+    <div class="single-testimonail">
+        <div class="testimonial-featured">
+            <img src="{{ URL::asset('images/fiverr.jpg') }}" style="border-radius: 50%;width: 128px; height: 128px;" />
+        </div>
+
+        <div class="testimonial-content">
+            <h3>Nancy</h3>
+            <p>I am loving CareNation, it is a very easy website to navigate and to get the help we need for our Mother. Neither my sister or I live close enough to Mum to be there to help her daily so we needed to find someone to be there for Mum. CareNation can provide just that! </p>
+        </div>
+    </div>
+</div>
+
 @endsection
